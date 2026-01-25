cae1a391b458859882b105696201c9ba942a3ec8
fatihtunali <fatihtunali@gmail.com>
2026-01-25 12:39:35 +0300

    Add Jest config for TypeScript tests

---
+++ server/jest.config.js
@@ -0,0 +1,15 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  verbose: true,
+  testTimeout: 30000,
+};
